3d08fd915f650fe983d286a2f6f36093d66906d4
Jean-Vitor <Jean.vitor@outlook.com.br>
2022-09-14 14:26:04 -0300

    Add eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Hello world')
-})
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(3000, () => {
   console.log('Server started at: http://localhost:3000');
-})
+});
